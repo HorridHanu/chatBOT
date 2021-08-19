e68f80f0031a98f4e97af3baeb3d61785bafcf50
HorridHanu <86579429+HorridHanu@users.noreply.github.com>
2021-08-19 14:55:17 +0530

    Add files via upload

---
+++ SplashScreen.java
@@ -0,0 +1,37 @@
+package com.hackerhanu.chatbot.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.hackerhanu.chatbot.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    private static int SPLASH_TIMER = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIMER);
+
+    }
+}
